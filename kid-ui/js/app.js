fd23011768629a573f937fc0058aeee1c15abb45
Eric Liao <rcliao01@gmail.com>
2016-10-22 12:59:56 -0700

    add angular router

--- kid-ui/js/app.js
+++ kid-ui/js/app.js
@@ -1,13 +1,50 @@
-angular.module('kid', []);
+angular.module('kid', ['ngComponentRouter']);
 
-console.log(AppCtrl);
+angular.module('kid')
+    .value('$routerRootComponent', 'kidApp');
+
+angular.module('kid')
+    .config(function($locationProvider) {
+        $locationProvider.html5Mode(true);
+    });
 
 angular.module('kid')
     .component('kidApp', {
         templateUrl: 'partials/app.html',
-        controller: AppCtrl
+        controller: AppCtrl,
+        $routeConfig: [
+            {
+                path: '/map', 
+                name: 'Map', 
+                component: 'kidMap',
+                useAsDefault: true
+            },
+            {
+                path: '/profile',
+                name: 'Profile',
+                component: 'kidProfile'
+            }
+        ]
+    });
+angular.module('kid')
+    .component('kidMap', {
+        templateUrl: 'partials/map.html',
+        controller: MapCtrl
+    });
+angular.module('kid')
+    .component('kidProfile', {
+        template: `Hello kid profile`,
+        controller: ProfileCtrl
     });
 
 function AppCtrl() {
     console.debug('Hello kid app');
+}
+
+function MapCtrl() {
+    console.debug('Hello map component');
+}
+
+function ProfileCtrl() {
+    console.debug('Hello profile component');
 }
