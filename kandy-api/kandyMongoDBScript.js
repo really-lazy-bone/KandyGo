a867434815b800647a3c77dcb0cf76c32c095c6a
Michael Hsu <pig0826usanj@gmail.com>
2016-10-22 12:12:01 -0700

    inistial mongodb script

---
+++ kandy-api/kandyMongoDBScript.js
@@ -0,0 +1,43 @@
+//kandy type data
+var kandyTypeData = [
+  {
+    "eventName": "halloween",
+    "eventDisplayName": "Halloween",
+    "eventDescription": "spooky fun festival for kids and adults",
+    "candyType": "bite-size-candy",
+    "candyName": "Bite-Sized Candy",
+    "candyImageName": "bite-size.svg",
+    "candyDescription": "cheap candy from cheap people lol"
+    "candyConversionRate": 0.05
+  },
+  {
+    "eventName": "halloween",
+    "eventDisplayName": "Halloween",
+    "eventDescription": "spooky fun festival for kids and adults",
+    "candyType": "normal-candy",
+    "candyName": "Normal Candy",
+    "candyImageName": "normal-size.svg",
+    "candyDescription": "your everyday standard candy",
+    "candyConversionRate":0.10
+  },
+  {
+    "eventName": "halloween",
+    "eventDisplayName": "Halloween",
+    "eventDescription": "spooky fun festival for kids and adults",
+    "candyType": "full-size-candy",
+    "candyName": "Full-Size Candy",
+    "candyImageName": "full-size.svg",
+    "candyDescription": "only the rich and elite can afford this. Full-sized bars",
+    "candyConversionRate":0.5
+  }
+];
+
+var kandydb = connect("127.0.0.1:27017/kandygo"),
+db.kandytypes.remove({});
+
+//create the names collection and add documents to it
+kandyTypeData.forEach(function(kandyType){
+  db.kandytypes.insert(kandyType);
+});
+
+db.kandytypes.find({});
